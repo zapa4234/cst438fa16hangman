97f72d7e141b92fbc4a763af1879046d5a8cc8c0
Soren Lassen <soren@fb.com>
2016-08-30 17:02:01 -0700

    Initial commit.
    The output of running
    
      mvn -B archetype:generate \
      -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-quickstart \
      -DgroupId=edu.csumb.cst438fa16hangman -DartifactId=cst438fa16hangman

---
+++ src/main/java/edu/csumb/cst438fa16hangman/App.java
@@ -0,0 +1,13 @@
+package edu.csumb.cst438fa16hangman;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
